6fcaa9e26ade2526ececc714dd181a028fd67c18
shengleizhu <zshenglei@163.com>
2023-03-22 14:39:03 +0800

    test

--- ods/ods_activity_info_full.sql
+++ ods/ods_activity_info_full.sql
@@ -1,14 +1,12 @@
-create external table ods_activity_info_full
+create  table if not exists ods.ods_activity_info_full
 （
-'id' string  comment '活动ID'
-'activity_name' string comment '活动名称'
-'activity_type' string comment '活动类型'
-'activity_desc' string comment '活动描述'
-'star time' string comment '开始时间'
-'end time' string comment '结束时间'
-'creat time' string comment '创建时间'
-） comment '活动信息表'
-partitioned by ('dt' string)
-row format delimited fields terminated by '\t'
-null defined as ' '
-location '/warehouse/gmall/ods/ods_activity_info_full/';
+    id                  STRING COMMENT '活动ID'
+    ，activity_name     STRING COMMENT '活动名称'
+    ，activity_type     STRING COMMENT '活动类型'
+    ，activity_desc     STRING COMMENT '活动描述'
+    ，start_time        STRING COMMENT '开始时间'
+    ，end_time          STRING COMMENT '结束时间'
+    ，create_time       STRING COMMENT '创建时间'
+） COMMENT '活动信息表'
+PARTITIONED by ('dt' STRING)
+stored as orc;
